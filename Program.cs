dd298472d0c778263a8d614e5b0f72dbb53547b8
anand acchu <you@example.com>
2022-08-28 18:25:26 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExceptionRegistration
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                int[] a = new int[3];
+
+                a[0] = 1;
+                a[1] = 2;
+                a[2] = 34;
+
+                int c = 11, b = 10;
+                int d = c / b;
+
+                Console.WriteLine("LineExcueted");
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Console.WriteLine(a[i]);
+                }
+
+
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                console.writeline(e.Message);
+            }
+        }
+    }
+}
